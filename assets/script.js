635a9d6c3bcedecc1cd8afbd78de38f9c80bd8ab
skyler <skyler@skylers-MacBook-Pro.local>
2023-06-03 13:07:51 -0300

    JS answers started

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,13 @@ var reset = document.getElementById("reset")
 var highScore = document.getElementById("highScore");
 var startPage = document.getElementById("startPage");
 var quizPage = document.getElementById("quizPage");
+
 var questionText = document.getElementById("question-text");
 var buttonList = document.getElementById("button-list");
 var trueAnswer = document.querySelectorAll("true");
 var falseAnswer = document.querySelectorAll("false");
 var currentQuestionIndex = 0;
+var currentAnswerIndex = 0, 1, 2, 3;
 trueAnswer = true;
 falseAnswer = false;
 
@@ -22,16 +24,28 @@ var questions =[
         questionAnswers: ["call myFunction()", "myFunction()", "call.myFunction()", "call function myFunction()"],
         correctAnswer: "myFunction()"
     },
-
-
+    {
+        questionText: "How do you create a function in JavaScript?",
+        questionAnswers: ["function = myFunction()", "function:myFunction()", "function myFunction()", "function-myFunction()"],
+        correctAnswer: "function myFunction()"
+    },
+    {
+        questionText: "How to write an IF statement for executing some code if 'i' is NOT equal to 27?",
+        questionAnswers: ["if (i <> 27)", "if i <> 27", "if i =! 27", "if (i != 27)"],
+        correctAnswer: "if (i != 27)"
+    },
+    {
+        questionText: "How does a FOR loop start?",
+        questionAnswers: ["for (i = 0; i <= 5)", "for (i ,=5; i++)", "for i = 1 to 5", "for (i = 0; i <= 5; i++)"],
+        correctAnswer: "for (i = 0; i <= 5; i++)"
+    }
 ];
 
 
 
 startButton.addEventListener("click", generateQuiz);
 highScore.addEventListener("click", viewHighScore);
-trueAnswer.addEventListener("click", trueAnswer.style.backgroundColor="green");
-
+//trueAnswer.addEventListener("click", trueAnswer.style.backgroundColor="green");
 
 //add a timer 
 
@@ -49,13 +63,29 @@ function renderQuestion() {
     for (var i = 0; i < choices.length; i++) {
         var choiceText = choices[i];
         console.log(choiceText);
+       
         // 1 - TODO: Create a new button
+
+        var currentAnswerObject = choiceText[currentAnswerIndex]
+        questionAnswers.textContent = currentAnswerObject.questionAnswers;
+        
+
+
 
         // 2 - TODO: Set the text of the button to be the choice text
 
+        
+       
+
+
         // 4 - TODO: Add a click event listener to the button that console logs "Button was clicked"
 
+        console.log("Button was clicked");
+
         // 3 - TODO: Append new button to button list element
+        
+        buttonList.append(buttonEl);
+    
     }
 
 
